5a20bfbd371902178e9a5dca935c7307d3c67580
Djole <79225724+djordjeerkicecka@users.noreply.github.com>
2023-01-25 00:16:32 +0100

    Global variables set, welcome view styled, grid view styled

---
+++ assets/scripts/main.js
@@ -0,0 +1,71 @@
+// Card Origin Object
+const CARDS_SOURCE = {
+	card1: {
+		id: 1,
+		src: './assets/img/blue.png'
+	},
+	card2: {
+		id: 2,
+		src: './assets/img/black.png'
+	},
+	card3: {
+		id: 3,
+		src: './assets/img/brown.png'
+	},
+	card4: {
+		id: 4,
+		src: './assets/img/cyan.png'
+	},
+	card5: {
+		id: 5,
+		src: './assets/img/dark_blue.png'
+	},
+	card6: {
+		id: 6,
+		src: './assets/img/gray.png'
+	},
+	card6: {
+		id: 7,
+		src: './assets/img/green.png'
+	},
+	card6: {
+		id: 8,
+		src: './assets/img/jade.png'
+	},
+	card6: {
+		id: 9,
+		src: './assets/img/lime.png'
+	},
+	card6: {
+		id: 10,
+		src: './assets/img/orange.png'
+	},
+	card6: {
+		id: 11,
+		src: './assets/img/pink.png'
+	},
+	card6: {
+		id: 12,
+		src: './assets/img/purple.png'
+	},
+	card6: {
+		id: 13,
+		src: './assets/img/red.png'
+	},
+	card6: {
+		id: 14,
+		src: './assets/img/violet.png'
+	},
+	card6: {
+		id: 15,
+		src: './assets/img/white.png'
+	},
+	card6: {
+		id: 16,
+		src: './assets/img/yellow.png'
+	}
+};
+
+const SETTINGS_EASY = 8;
+const SETTINGS_MEDIUM = 12;
+const SETTINGS_HARD = 16;
